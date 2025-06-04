3a7221c6b23f883abf524c2884bb203d548f7387
DarwinDO <nguyendo836660@gmail.com>
2025-06-04 22:12:14 +0700

    feat(fe): :sparkles: add a part of update user

--- apps/learn-react-app-DoNHV/src/component/layout/header.jsx
+++ apps/learn-react-app-DoNHV/src/component/layout/header.jsx
@@ -1,19 +1,35 @@
 import { Link, NavLink } from 'react-router-dom';
-import './header.css';
+import { Menu } from "antd";
+import { HomeOutlined, UserOutlined, BookOutlined } from '@ant-design/icons';
+import { useState } from "react";
 
 const Header = () => {
+
+  const items = [
+    {
+      label: <Link to="/">Home</Link>,
+      key: 'home',
+      icon: <HomeOutlined />,
+    },
+    {
+      label: <Link to="/users">User</Link>,
+      key: 'users',
+      icon: <UserOutlined />,
+    },
+    {
+      label: <Link to="/books">Books</Link>,
+      key: 'books',
+      icon: <BookOutlined />,
+    },
+  ];
+
+  const [current, setCurrent] = useState('');
+  const onClick = e => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  }
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/users">Users</NavLink>
-      </li>
-      <li>
-        <NavLink to="/books">Books</NavLink>
-      </li>
-    </ul>
+    <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
   );
 };
 
