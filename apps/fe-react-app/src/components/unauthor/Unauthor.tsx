3bfd70da47dd8d19f2a8a8653493ee93edae9ad8
baole405 <baolttse184569@fpt.edu.vn>
2025-06-05 16:04:05 +0700

    feat(fe): unauthor components

---
+++ apps/fe-react-app/src/components/unauthor/Unauthor.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Unauthorized = () => {
+  return (
+    <div>
+      <h1>Unauthorized Access</h1>
+      <p>You do not have permission to access this page.</p>
+      <Link to="/">Return to Home</Link>
+    </div>
+  );
+};
+
+export default Unauthorized;
+
