41bfdaa3813ea2fd62cb3d7ca21f83b61e1cec2f
baole405 <baolttse184569@fpt.edu.vn>
2025-06-17 15:02:27 +0700

    feat(fe): fuzzyText for 404

--- apps/fe-react-app/src/components/error/NotFoundError.tsx
+++ apps/fe-react-app/src/components/error/NotFoundError.tsx
@@ -1,3 +1,4 @@
+import FuzzyText from "../../../Reactbits/FuzzyText/FuzzyText";
 import NavigateButton from "../../components/shared/NavigateButton";
 
 const NotFoundError = () => {
@@ -5,9 +6,12 @@ const NotFoundError = () => {
     <section className="flex h-full items-center p-16 dark:bg-gray-50 dark:text-gray-800">
       <div className="container mx-auto my-8 flex flex-col items-center justify-center px-5">
         <div className="flex max-w-md flex-col items-center justify-center text-center">
-          <h2 className="mb-8 text-9xl font-extrabold dark:text-gray-400">
-            <span className="sr-only">Error</span>404
-          </h2>
+          <div className="mb-8">
+            <span className="sr-only">Error</span>
+            <FuzzyText fontSize="9rem" fontWeight={800} color="#D2935A" baseIntensity={0.2} hoverIntensity={0.6}>
+              404
+            </FuzzyText>
+          </div>
           <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
           <p className="mb-8 mt-4 dark:text-gray-600">But don't worry, you can find plenty of other things on our homepage.</p>
           <NavigateButton to="/" text="Back to Home" />
