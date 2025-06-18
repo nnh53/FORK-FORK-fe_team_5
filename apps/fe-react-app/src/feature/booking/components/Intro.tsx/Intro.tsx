7c36910539913a3e72fff4822b1cb1eab9460c66
baole405 <baolttse184569@fpt.edu.vn>
2025-06-18 18:42:21 +0700

    feat: ready to apply scrolltrigger

---
+++ apps/fe-react-app/src/feature/booking/components/Intro.tsx/Intro.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>Experience Cinema Like Never Before</h1>
+      <p>Immerse yourself in stunning visuals and captivating stories</p>
+    </div>
+  )
+}
+
+export default Intro
