1e99b2a3bfba0613ae955cb07c71356d87741c8a
Dev Wibu <tantnse184055@fpt.edu.vn>
2025-06-24 23:28:23 +0700

    fix(fe): update staff interface

--- apps/fe-react-app/src/interfaces/staff.interface.ts
+++ apps/fe-react-app/src/interfaces/staff.interface.ts
@@ -1,12 +1,8 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import type { UserBase } from "./users.interface";
 
-export enum StaffStatus {
-  VERIFY = "VERIFY",
-  BAN = "BAN",
-  UNVERIFY = "UNVERIFY",
-}
+export type STAFF_STATUS = "ACTIVE" | "UNVERIFY" | "BAN";
 
 export interface Staff extends UserBase {}
 
-export type StaffFormData = Omit<Staff, "staff_id">;
+export type StaffFormData = Omit<Staff, "id">;
