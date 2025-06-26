d8f8f910b1005713a7af451620f76946bd7dc243
Dev Wibu <tantnse184055@fpt.edu.vn>
2025-06-27 05:23:54 +0700

    fix: correct room_id and  ShowtimeStatus

--- apps/fe-react-app/src/interfaces/showtime.interface.ts
+++ apps/fe-react-app/src/interfaces/showtime.interface.ts
@@ -1,10 +1,16 @@
+export enum ShowtimeStatus {
+  SCHEDULE = "SCHEDULE",
+  ONSCREEN = "ONSCREEN",
+  COMPLETE = "COMPLETE",
+}
+
 export interface Showtime {
   id: string;
   movie_id: string;
-  room_Id: string;
+  room_id: string;
   show_date_time: string;
   show_end_time: string;
   status: ShowtimeStatus;
 }
 
-export type ShowtimeStatus = "SCHEDULE" | "ONSCREEN" | "COMPLETE";
+export type ShowtimeFormData = Omit<Showtime, "id">;
