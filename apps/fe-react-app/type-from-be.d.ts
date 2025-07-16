82c97f5c202185392ab3c9f40aaea1c24d56a538
Nguyễn Nam Hoàng <nnh53.work@gmail.com>
2025-07-16 22:24:31 +0700

    feat: add schema comparison script

--- apps/fe-react-app/type-from-be.d.ts
+++ apps/fe-react-app/type-from-be.d.ts
@@ -1,6 +1,6 @@
 import type { components } from "./schema-from-be";
 
-export type CustomAPIResponse = components["schemas"]["CustomAPIResponse"];
+export type CustomAPIResponse = components["schemas"]["ApiResponseVoid"];
 export type UserResponse = components["schemas"]["UserResponse"];
 export type UserUpdate = components["schemas"]["UserUpdate"];
 export type AuthenticationResponse = components["schemas"]["AuthenticationResponse"];
@@ -10,7 +10,7 @@ export type CinemaRoomUpdateRequest = components["schemas"]["CinemaRoomUpdateReq
 export type SeatResponse = components["schemas"]["SeatResponse"];
 export type SeatRequest = components["schemas"]["SeatRequest"];
 export type SeatTypeResponse = components["schemas"]["SeatTypeResponse"];
-export type PageResponse = components["schemas"]["PageResponse"];
+export type PageResponseShowtimeResponse = components["schemas"]["PageResponseShowtimeResponse"];
 export type PromotionResponse = components["schemas"]["PromotionResponse"];
 export type ShowtimeResponse = components["schemas"]["ShowtimeResponse"];
 export type MovieResponse = components["schemas"]["MovieResponse"];
